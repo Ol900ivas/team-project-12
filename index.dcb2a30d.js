4fca495557000de7b2bddfd4187ec7b189f9a95a
Ol900ivas <112936701+Ol900ivas@users.noreply.github.com>
2022-11-27 17:31:27 +0000

    Deploying to gh-pages from @ Ol900ivas/team-project-12@8edb51ed5d27df1a6a056004c04ef3ea0a132ec6 🚀

--- index.dcb2a30d.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.dcb2a30d.js.map
