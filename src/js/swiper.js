f3879fdc9c9a20b858d819e34e97a7c98dd32538
kravets-julia <kravets_julia@i.ua>
2022-11-27 19:12:12 +0200

    slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,20 @@
-const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     //   direction: ‘vertical’,
     loop: true,
     // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
+      el: '.swiper-pagination',
       clickable: true, // что-бы сделать пагинацию кликабельной
     },
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
     // And if we need scrollbar
     scrollbar: {
-        el: '.swiper-scrollbar',
+      el: '.swiper-scrollbar',
     },
   });
